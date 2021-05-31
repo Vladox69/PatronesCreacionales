902c10f5a758e5c80fe12e4ad9c4ccc7c32a012d
OppositeDragon <stee-ven_94@outlook.com>
2021-05-30 19:03:09 -0500

    Update PlanInstitucional.java

--- src/main/java/cartera/PlanInstitucional.java
+++ src/main/java/cartera/PlanInstitucional.java
@@ -13,7 +13,7 @@ public class PlanInstitucional implements Plan{
 
     @Override
     public String getDatos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "Plan institucional";
     }
     
 }
